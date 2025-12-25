047b3a284235611b8ae320a57fef13b4e58492f3
royzchow <roychow123@gmail.com>
2025-12-26 00:45:44 +0800

    css

--- layouts/Baseof.js
+++ layouts/Baseof.js
@@ -30,6 +30,11 @@ const Base = ({
 
         <meta name="color-scheme" content="light only" />
         <meta name="supported-color-schemes" content="light" />
+        <meta name="theme-color" content="#ffffff" />
+
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="theme-color" content="#ffffff" />
 
         {/* canonical url */}
         {canonical && <link rel="canonical" href={canonical} itemProp="url" />}
